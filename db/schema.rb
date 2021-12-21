4e6c6abe27c75851a50a5401caff8539356b8639
EC2 Default User <ec2-user@ip-172-31-25-212.ec2.internal>
2021-12-21 05:52:59 +0000

    topページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_090154) do
+ActiveRecord::Schema.define(version: 2021_12_21_054215) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "category"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "homes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
